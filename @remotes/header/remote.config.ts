5614d9082caca458845dc3899fe8bdb42abdea1b
michalmatusik <michal.matusik@docplanner.com>
2024-03-05 13:43:25 +0100

    fix

--- @remotes/header/remote.config.ts
+++ @remotes/header/remote.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from '@docplanner/remotejs';
 
 export default defineConfig(() => ({
   name: 'header',
+  translations: {
+    entry: './.i18n/*.json',
+  },
   moduleFederationConfig: {
     exposes: {
       './app': './src/index',
